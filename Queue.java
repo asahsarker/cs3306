156854dc774f3eff5941cf5f16fe91bac227d835
asahsarker <asahsarker@gmail.com>
2022-08-11 19:04:48 +0100

    updates on queue class

---
+++ Queue.java
@@ -0,0 +1,35 @@
+public class Queue {
+
+    private int[] arr;  
+    private int count;
+    private int front = 0;
+    
+    public Queue() {
+        front = 0;
+        count = 0;
+       
+    }
+
+    public int size() {
+        return count;
+    }
+    public int add() {
+        return 0;
+    } 
+    
+    public int element() {
+        return 0;
+    } 
+    
+    public int peek() {
+        return 0;
+    } 
+    
+    public int poll() {
+        return 0;
+    }
+
+    public int remove() {
+        return 0;
+    }
+}
